ddc9173ce716b4aa61adee3225a624f5d02c843f
Niels Overwijn <niels.overwijn@gmail.com>
2017-08-31 21:49:33 +0200

    Pencil fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var scrollDownButton = document.getElementById('scroll-down-button');
 var socialIconsTop = document.getElementById("social-media-top");
 var socialIconsFooter = document.getElementById("social-media-footer");
+var pencilDashTop = document.getElementById("pencil-dash-top");
+var pencilDashBottom = document.getElementById("pencil-dash-bottom");
 var feather = document.getElementById("feather");
 var animationInterval = [];
 var docElm = document.documentElement;
@@ -31,6 +33,8 @@ function scrollHandler() {
     socialIconsTop.style.opacity = 1 - pos;
     socialIconsFooter.style.opacity = pos;
     scrollDownButton.style.opacity = 1 - pos;
+    pencilDashTop.style.opacity = 1 -pos;
+    pencilDashBottom.style.opacity = pos;
     feather.style.top = (30 + (pos * 5)).toString() + "%";
     feather.style.left = (20 - (pos * 2)).toString() + "%";
     feather.style.transform = "rotate(" + (-15 + (15 * pos)).toString() + "deg)";
@@ -38,3 +42,6 @@ function scrollHandler() {
 
 socialIconsTop.style.opacity = 1 - pos;
 socialIconsFooter.style.opacity = pos;
+
+pencilDashTop.style.opacity = 1 -pos;
+pencilDashBottom.style.opacity = pos;
