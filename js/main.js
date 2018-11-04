35b49c63eb8498ea6a185bfa96f4121a17d5b7bf
Niels Overwijn <niels.overwijn@gmail.com>
2018-11-04 14:01:16 +0100

    First draft of 2018 site

--- js/main.js
+++ js/main.js
@@ -1,47 +1,21 @@
-var scrollDownButton = document.getElementById('scroll-down-button');
-var socialIconsTop = document.getElementById("social-media-top");
-var socialIconsFooter = document.getElementById("social-media-footer");
-var pencilDashTop = document.getElementById("pencil-dash-top");
-var pencilDashBottom = document.getElementById("pencil-dash-bottom");
-var feather = document.getElementById("feather");
-var animationInterval = [];
-var docElm = document.documentElement;
-var pos = ( document.body.scrollTop || docElm.scrollTop || docElm.scrollTop) / ( docElm.scrollHeight - (docElm.clientHeight + 50) );
-
-var hammertime = new Hammer(scrollDownButton);
-hammertime.on('tap', clickHandler);
-
-
-window.addEventListener('scroll', scrollHandler);
-
-function clickHandler(e) {
-    animationInterval.push(setInterval(scrollToBottom, 5));
+const objects = {
+    scroll_btn: document.getElementById('scroll-down-btn')
 }
 
-function scrollToBottom() {
-    pos = ( document.body.scrollTop || docElm.scrollTop || docElm.scrollTop) / ( docElm.scrollHeight - (docElm.clientHeight + 20) );
-    if (pos >= 1) {
-        for(var i = 0; i < animationInterval.length; i++) {
-            clearInterval(animationInterval[i]);
-        }
-    } else {
-        document.body.scrollTop += 10;
-        docElm.scrollTop += 20;
-    }
+function getScrollPercent() {
+    return (document.documentElement.scrollTop || document.body.scrollTop) / ((document.documentElement.scrollHeight || document.body.scrollHeight) - document.documentElement.clientHeight);
 }
 
-function scrollHandler() {
-    pos = ( document.body.scrollTop || docElm.scrollTop || docElm.scrollTop) / ( docElm.scrollHeight - (docElm.clientHeight + 20) );
-    socialIconsTop.style.opacity = 1 - pos;
-    socialIconsFooter.style.opacity = pos;
-    scrollDownButton.style.opacity = 1 - pos;
-    pencilDashTop.style.opacity = 1 -pos;
-    pencilDashBottom.style.opacity = pos;
-    feather.style.transform = "rotate(" + (-15 + (15 * pos)).toString() + "deg)";
+function setScrollButtonOpacity () {
+    objects.scroll_btn.style.opacity = 1 - getScrollPercent();
 }
 
-socialIconsTop.style.opacity = 1 - pos;
-socialIconsFooter.style.opacity = pos;
+document.addEventListener("scroll", setScrollButtonOpacity);
+objects.scroll_btn.addEventListener('click', () => {
+    window.scroll({
+        top: (document.documentElement.scrollHeight || document.body.scrollHeight),
+        behavior: 'smooth'
+      });
+});
 
-pencilDashTop.style.opacity = 1 -pos;
-pencilDashBottom.style.opacity = pos;
+setScrollButtonOpacity();
